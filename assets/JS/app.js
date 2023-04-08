8a9b35774e52efe4e8954bd8f7917cfafd4d23a2
Nazilamf <nazilamf@code.edu.az>
2023-04-09 00:36:55 +0400

    ABB-header

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,10 +1,25 @@
 let search = document.querySelector('#search');
 let div = document.querySelector('#help');
+let icon = document.querySelector('#icon')
 
 
 search.onclick = function(){
-    div.classList.add('.active')
+    div.classList.toggle('active');
+    
+
+}
+
+icon.onclick = function(){
+    div.classList.toggle('active')
+    
 }
 
 
+let language = document.querySelector('#language')
+let select_lang =document.querySelector('.select-lang')
+let transform_ = document.querySelector('#transform')
 
+select_lang.onclick = function(){
+    language.classList.toggle('active')
+    transform_.classList.toggle('active_transform')
+}
